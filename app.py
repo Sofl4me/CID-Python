3dfd6ea2a26c2715a064af94d821075e44e9f333
Sofl4me <sonny.grisorio@supdevinci-edu.fr>
2024-10-25 16:30:23 +0200

    Update app.py
    Update

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from health_utils import calculate_bmi, calculate_bmr
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "HealtApp Welcome Page !"
+
 @app.route('/bmi', methods=['POST'])
 def bmi():
     data = request.json
